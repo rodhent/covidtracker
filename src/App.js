9fe74af1a53e353d843033e2a345bb8158235ab9
Rod Hentringer <67014030+rodhent@users.noreply.github.com>
2020-11-24 19:13:46 -0500

    app update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-import { Cards, Chart, CountryPicker } from './components'
+import { Cards, Chart, CountryPicker } from './components/'
 import styles from './App.module.css'
+import { fetchData } from './api'
 
 class App extends React.Component {
+
+  async componentDidMount() {
+    const data = await fetchData()
+
+    console.log(data)
+  }
+
   render() {
     return (
       <div className={styles.container}>
@@ -14,3 +22,5 @@ class App extends React.Component {
     )
   }
 }
+
+export default App
