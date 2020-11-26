a48b46a5896c34f43f6fbd4ec46df9d57d959271
Rod Hentringer <67014030+rodhent@users.noreply.github.com>
2020-11-25 23:29:35 -0500

    cards update

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
-const Cards = () => {
+import { Card, CardContent, Typography, Grid } from '@material-ui/core'
+
+import styles from './Cards.module.css'
+
+const Cards = (props) => {
+    console.log(props)
+
     return (
         <div>
             <h1>Cards</h1>
